74471abfe85af2b6d61c624e66b6ca547e18b683
optimisme <optimisme@gmail.com>
2023-01-26 17:52:54 +0100

    Revert "Afegit autor"
    This reverts commit 0c38f049c379f9195cb791660f970cb10e81b629.

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Villan
- */
 public class Main extends Application {
 
     public static void main(String[] args) {
