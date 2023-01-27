ea55feda71e132c5ad67742a598f4c49e262c8c7
optimisme <optimisme@gmail.com>
2023-01-27 09:03:08 +0100

    Unified views behaviour

---
+++ src/Controller0.java
@@ -0,0 +1,53 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Villan
+ */
+public class Controller0 implements Initializable {
+
+    @FXML
+    private Button button0, button1, button2;
+    @FXML
+    private AnchorPane container;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+
+    @FXML
+    private void toView0(ActionEvent event) {
+        Main.setView("View0");
+    }
+    
+    @FXML
+    private void toView1(ActionEvent event) {
+        Main.setView("View1");
+    }
+
+    @FXML
+    private void toView2(ActionEvent event) {
+        Main.setView("View2");
+    }
+
+    @FXML
+    private void animateToView0(ActionEvent event) {
+        Main.setViewAnimating("View0");
+    }
+    
+    @FXML
+    private void animateToView1(ActionEvent event) {
+        Main.setViewAnimating("View1");
+    }
+
+    @FXML
+    private void animateToView2(ActionEvent event) {
+        Main.setViewAnimating("View2");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
 
 
+import java.util.ArrayList;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
+
+    private static Stage primaryStage;
+    private static StackPane views = new StackPane();
 
     public static void main(String[] args) {
         launch(args);
@@ -15,15 +27,116 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-        StackPane root = new StackPane();
-        root.setId("parentContainer");
+        views.setId("parentContainer");
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("./assets/scene0.fxml"));
-        root.getChildren().add(loader.load());
-        
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Animació entre escenes");
-        stage.show();
+        addView(getClass(), "View0", "./assets/view0.fxml", true);
+        addView(getClass(), "View1", "./assets/view1.fxml", false);
+        addView(getClass(), "View2", "./assets/view2.fxml", false);
+
+        Scene scene = new Scene(views);
+
+        primaryStage = stage;
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Animació entre escenes");
+        primaryStage.show();
+    }
+
+    // Add one view to the list
+    public static void addView(Class<?> cls, String name, String path, boolean defaultView) throws Exception {
+        FXMLLoader loader = new FXMLLoader(cls.getResource(path));
+        Pane view = loader.load();
+
+        view.setId(name);
+        view.setVisible(defaultView);
+        view.setManaged(defaultView);
+
+        views.getChildren().add(view);
+    }
+
+    // Set visible view by its id (viewId)
+    public static void setView(String viewId) {
+
+        // Get root of the scene
+        StackPane root = (StackPane) primaryStage.getScene().getRoot();
+
+        ArrayList<Node> list = new ArrayList<>();
+        list.addAll(root.getChildrenUnmodifiable());
+
+        // Show next view, hide others
+        for (Node n : list) {
+            n.translateXProperty().set(0); // Reset translation
+            if (n.getId().equals(viewId)) {
+                n.setVisible(true);
+                n.setManaged(true);
+            } else {
+                n.setVisible(false);
+                n.setManaged(false);
+            }
+        }
+
+        // Remove focus from buttons
+        root.requestFocus();
+    }
+
+    // Set visible view by its id (viewId) with an animation
+    public static void setViewAnimating(String viewId) {
+
+        // Get root of the scene
+        StackPane root = (StackPane) primaryStage.getScene().getRoot();
+
+        ArrayList<Node> list = new ArrayList<>();
+        list.addAll(root.getChildrenUnmodifiable());
+
+        // Get current view
+        Node curView = null;
+        for (Node n : list) {
+            n.translateXProperty().set(0); // Reset translation
+            if (n.isVisible()) {
+                curView = n;
+            }
+        }
+
+        // Get nxtView
+        Node nxtView = null;
+        for (Node n : list) {
+            if (n.getId().equals(viewId)) {
+                nxtView = n;
+            }
+        }
+
+        double xStart = primaryStage.getScene().getWidth();
+        double xEnd = 0;
+        Node animatedView = nxtView;
+
+        // If curView is over nxtView
+        if (list.indexOf(curView) > list.indexOf(nxtView)) {
+            xStart = 0;
+            xEnd = primaryStage.getScene().getWidth();
+            animatedView = curView;
+        }
+
+        // Set nxtView visible and translate it to the left
+        nxtView.setVisible(true);
+        nxtView.setManaged(true);
+        nxtView.translateXProperty().set(xStart);
+
+        // Animate curView from left to right
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(animatedView.translateXProperty(), xEnd, Interpolator.EASE_BOTH);
+        KeyFrame kf = new KeyFrame(Duration.seconds(0.3), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            // Hide other views
+            for (Node n : list) {
+                if (!n.getId().equals(viewId)) {
+                    n.setVisible(false);
+                    n.setManaged(false);
+                }
+            }
+        });
+        timeline.play();
+
+        // Remove focus from buttons
+        root.requestFocus();
     }
 }
