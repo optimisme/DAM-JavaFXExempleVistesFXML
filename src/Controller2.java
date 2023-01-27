608d05bd85fcc8bdcc705e3c2ad4f8615c91a03d
optimisme <optimisme@gmail.com>
2023-01-27 16:20:57 +0100

    Added insert examples

--- src/Controller2.java
+++ src/Controller2.java
@@ -1,17 +1,25 @@
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class Controller2 implements Initializable {
 
     @FXML
-    private Button button0, button1, button2;
+    private Button button0, button1, button2, buttonWeekdays, buttonMonths, buttonAnimals, buttonBrands;
     @FXML
     private AnchorPane container;
+    @FXML
+    private ChoiceBox<String> choiceBox = new ChoiceBox<>();
+    @FXML
+    private VBox yPane = new VBox();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -47,4 +55,66 @@ public class Controller2 implements Initializable {
         Main.setViewAnimating("View2");
     }
 
+    @FXML
+    private void setWeekdays (ActionEvent event) {
+        choiceBox.getItems().clear();
+        choiceBox.getItems().addAll("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+        choiceBox.setValue("Monday");
+    }
+
+    @FXML
+    private void setMonths (ActionEvent event) {
+        choiceBox.getItems().clear();
+        choiceBox.getItems().addAll("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        choiceBox.setValue("January");
+    }
+
+    @FXML
+    private void setAnimals (ActionEvent event) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Dog");
+        list.add("Cat");
+        list.add("Horse");
+        list.add("Cow");
+        list.add("Pig");
+
+        yPane.getChildren().clear();
+        for (String s : list) {
+            Label label = new Label(s);
+            label.setStyle("-fx-border-color: black;");
+            yPane.getChildren().add(label);
+        }
+    }
+
+    @FXML
+    private void setBrands (ActionEvent event) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Audi");
+        list.add("BMW");
+        list.add("Citroen");
+        list.add("Fiat");
+        list.add("Ford");
+        list.add("Honda");
+        list.add("Hyundai");
+        list.add("Kia");
+        list.add("Mazda");
+        list.add("Mercedes");
+        list.add("Nissan");
+        list.add("Opel");
+        list.add("Peugeot");
+        list.add("Renault");
+        list.add("Seat");
+        list.add("Skoda");
+        list.add("Suzuki");
+        list.add("Toyota");
+        list.add("Volkswagen");
+        list.add("Volvo");
+
+        yPane.getChildren().clear();
+        for (String s : list) {
+            Label label = new Label(s);
+            label.setStyle("-fx-border-color: black;");
+            yPane.getChildren().add(label);
+        }
+    }
 }
