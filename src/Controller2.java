c802c525542698ebe17006090ca90c5c5a840b57
optimisme <optimisme@gmail.com>
2023-01-28 11:24:07 +0100

    Add initializer

--- src/Controller2.java
+++ src/Controller2.java
@@ -1,8 +1,11 @@
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -10,7 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-public class Controller2 {
+public class Controller2 implements Initializable {
 
     @FXML
     private Button button0, button1, button2, buttonWeekdays, buttonMonths, buttonAnimals, buttonBrands, buttonFXML;
@@ -20,6 +23,21 @@ public class Controller2 {
     private ChoiceBox<String> choiceBox = new ChoiceBox<>();
     @FXML
     private VBox yPane = new VBox();
+
+    String fruits[] = { "Apple", "Banana", "Cherry", "Grape", "Kiwi", "Lemon", "Lime", "Mango", "Orange", "Peach", "Pear", "Pineapple", "Plum", "Strawberry" };
+    String weekdays[] = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+    String months[] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+    String animals[] = { "Dog", "Cat", "Horse", "Cow", "Pig" };
+    String brands[] = { "Audi", "BMW", "Citroen", "Fiat", "Ford", "Honda", "Hyundai", "Kia", "Mazda", "Mercedes", "Nissan", "Opel", "Peugeot", "Renault", "Seat", "Skoda", "Suzuki", "Toyota", "Volkswagen", "Volvo" };
+    
+    // Declare an array of arrays of strings
+    ArrayList<ArrayList<String>> list = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        choiceBox.getItems().addAll(fruits);
+        choiceBox.setValue("Apple");
+    }
 
     @FXML
     private void toView0(ActionEvent event) {
@@ -54,26 +72,23 @@ public class Controller2 {
     @FXML
     private void setWeekdays (ActionEvent event) {
         choiceBox.getItems().clear();
-        choiceBox.getItems().addAll("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+        choiceBox.getItems().addAll(weekdays);
         choiceBox.setValue("Monday");
     }
 
     @FXML
     private void setMonths (ActionEvent event) {
         choiceBox.getItems().clear();
-        choiceBox.getItems().addAll("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        choiceBox.getItems().addAll(months);
         choiceBox.setValue("January");
     }
 
     @FXML
     private void setAnimals (ActionEvent event) {
         ArrayList<String> list = new ArrayList<>();
-        list.add("Dog");
-        list.add("Cat");
-        list.add("Horse");
-        list.add("Cow");
-        list.add("Pig");
-
+        for (String name: animals) {
+            list.add(name);
+        }
         yPane.getChildren().clear();
         for (String s : list) {
             Label label = new Label(s);
@@ -85,26 +100,9 @@ public class Controller2 {
     @FXML
     private void setBrands (ActionEvent event) {
         ArrayList<String> list = new ArrayList<>();
-        list.add("Audi");
-        list.add("BMW");
-        list.add("Citroen");
-        list.add("Fiat");
-        list.add("Ford");
-        list.add("Honda");
-        list.add("Hyundai");
-        list.add("Kia");
-        list.add("Mazda");
-        list.add("Mercedes");
-        list.add("Nissan");
-        list.add("Opel");
-        list.add("Peugeot");
-        list.add("Renault");
-        list.add("Seat");
-        list.add("Skoda");
-        list.add("Suzuki");
-        list.add("Toyota");
-        list.add("Volkswagen");
-        list.add("Volvo");
+        for (String name: brands) {
+            list.add(name);
+        }
 
         yPane.getChildren().clear();
         for (String s : list) {
@@ -118,36 +116,45 @@ public class Controller2 {
     private void setFXML (ActionEvent event) throws Exception {
      
         // List of two strings with spices and animals
-        ArrayList<ArrayList<String>> list = new ArrayList<>();
-        list.add(new ArrayList<String>() { { add("Mamals"); add("Dog"); add("black"); } });
-        list.add(new ArrayList<String>() { { add("Mamals"); add("Cat"); add("grey"); } });
-        list.add(new ArrayList<String>() { { add("Mamals"); add("Horse"); add("brown"); } });
-        list.add(new ArrayList<String>() { { add("Mamals"); add("Cow"); add("white"); } });
-        list.add(new ArrayList<String>() { { add("Mamals"); add("Pig"); add("pink"); } });
-        list.add(new ArrayList<String>() { { add("Birds"); add("Pidgeon"); add("grey"); } });
-        list.add(new ArrayList<String>() { { add("Birds"); add("Duck"); add("white"); } });
-        list.add(new ArrayList<String>() { { add("Birds"); add("Chicken"); add("orange"); } });
-        list.add(new ArrayList<String>() { { add("Birds"); add("Goose"); add("white"); } });
-        list.add(new ArrayList<String>() { { add("Fish"); add("Salmon"); add("orange"); } });
-        list.add(new ArrayList<String>() { { add("Fish"); add("Tuna"); add("brown"); } });
-        list.add(new ArrayList<String>() { { add("Fish"); add("Cod"); add("grey"); } });
-        list.add(new ArrayList<String>() { { add("Fish"); add("Herring"); add("blue"); } });
-        list.add(new ArrayList<String>() { { add("Reptiles"); add("Snake"); add("green"); } });
-        list.add(new ArrayList<String>() { { add("Reptiles"); add("Lizard"); add("green"); } });
-        list.add(new ArrayList<String>() { { add("Reptiles"); add("Turtle"); add("brown"); } });
-        list.add(new ArrayList<String>() { { add("Reptiles"); add("Crocodile"); add("green"); } });
+        String[][] list = new String[][] { 
+            new String[] { "Mamals", "Dod", "black" }, 
+            new String[] { "Mamals", "Cat", "grey" },
+            new String[] { "Mamals", "Horse", "brown" },
+            new String[] { "Mamals", "Cow", "white" },
+            new String[] { "Mamals", "Pig", "pink" },
+            new String[] { "Birds", "Pidgeon", "grey" },
+            new String[] { "Birds", "Duck", "white" },
+            new String[] { "Birds", "Eagle", "brown" },
+            new String[] { "Birds", "Owl", "black" },
+            new String[] { "Birds", "Parrot", "green" },
+            new String[] { "Fish", "Goldfish", "orange" },
+            new String[] { "Fish", "Shark", "grey" },
+            new String[] { "Fish", "Tuna", "silver" },
+            new String[] { "Fish", "Salmon", "pink" },
+            new String[] { "Fish", "Cod", "white" },
+            new String[] { "Reptiles", "Snake", "black" },
+            new String[] { "Reptiles", "Lizard", "green" },
+            new String[] { "Reptiles", "Turtle", "brown" },
+            new String[] { "Reptiles", "Crocodile", "grey" },
+            new String[] { "Reptiles", "Alligator", "green" },
+            new String[] { "Amphibians", "Frog", "green" },
+            new String[] { "Amphibians", "Toad", "brown" },
+            new String[] { "Amphibians", "Salamander", "grey" },
+            new String[] { "Amphibians", "Newt", "brown" },
+            new String[] { "Amphibians", "Axolotl", "pink" }
+        };
 
         URL resource = this.getClass().getResource("./assets/listItem.fxml");
 
         yPane.getChildren().clear();
-        for (ArrayList<String> listElement : list) {
+        for (String[] listElement : list) {
             FXMLLoader loader = new FXMLLoader(resource);
             Parent itemTemplate = loader.load();
             ControllerListItem itemController = loader.getController();
             
-            itemController.setTitle(listElement.get(1));
-            itemController.setSubtitle(listElement.get(0));
-            itemController.setCircleColor(listElement.get(2));
+            itemController.setTitle(listElement[1]);
+            itemController.setSubtitle(listElement[0]);
+            itemController.setCircleColor(listElement[2]);
 
             yPane.getChildren().add(itemTemplate);
         }
